939980304a6129f2bb06fbf3de65686cca1a66e6
fordwright21 <fordwright@gmail.com>
2015-08-01 01:15:41 -0700

    Revert "Revert "Created server and client. Using threads to allow multiple clients to connect but two player isn't finished and server is still asynchronous so only one game can currently be played at a time.""
    This reverts commit f03baf704fd87adcdbb928ebf2127a043efe4ddc.

--- BattleShipServer.py
+++ BattleShipServer.py
@@ -1,23 +1,32 @@
+__author__ = 'CDawg'
+import socket
+import thread
+import struct
 from random import randint
 from random import choice
+
 
 class Ship(object):
     def __init__(self):
         self.hits = []
         self.location = []
-        self.status  = "alive"
+        self.status = "alive"
 
     def update_status(self):
         self.status = "dead"
 
+
 class SmallShip(Ship):
     size = 1
+
 
 class MediumShip(Ship):
     size = 2
 
+
 class LargeShip(Ship):
     size = 3
+
 
 class Board(object):
     def __init__(self, size):
@@ -59,7 +68,7 @@ class Board(object):
             "up": [point[0] - 1, point[1], 2],  # up
             "down": [point[0] + 1, point[1], 3]  # down
         }
-        return switcher.get(choice(direction), False)
+        return switcher.get(choice(direction))
 
     # Used to find a point that is adjacent to a given point
     def find_adjacent_space(self, point, hor_ver):
@@ -72,7 +81,8 @@ class Board(object):
             direction = self.select_direction(unused_directions, choice(point))
             while True:
                 try:
-                    if direction[0] <= -1 or direction[0] >= self.size or direction[1] <= -1 or direction[1] == self.size:
+                    if direction[0] <= -1 or direction[0] >= self.size or direction[1] <= -1 or \
+                            direction[1] == self.size:
                         del unused_directions[direction[3]]
                         direction = self.select_direction(choice(unused_directions), choice(point))
                     elif direction[0:2] in self.placed_ships or direction[0:2] in point:
@@ -84,7 +94,7 @@ class Board(object):
 
 
 class Player(object):
-    def __init__(self,name):
+    def __init__(self, name):
         self.sunk = []
         self.ships = []
         self.board = Board(7)
@@ -95,8 +105,8 @@ class Player(object):
         for ship in fleet:
             self.ships.append(ship)
 
-    # generate_ships loops through each ship in the list ships. Then it selects a ranndom point from the board. Saves that
-    # point and then finds adjacent points as needed by the ship's ship size.
+# generate_ships loops through each ship in the list ships. Then it selects a ranndom point from the board. Saves that
+# point and then finds adjacent points as needed by the ship's ship size.
     def generate_ships(self):
         for ship in self.ships:
             while True:
@@ -104,7 +114,7 @@ class Player(object):
                     point = self.board.find_free_space()
                     ship.location.append(point)
                     if ship.size > 1:
-                        direction = self.board.find_adjacent_space(ship.location,"any")
+                        direction = self.board.find_adjacent_space(ship.location, "any")
                         ship.location.append(direction[0:2])
                         if ship.size > 2:
                             for next_point in range(ship.size - 2):
@@ -136,78 +146,122 @@ class Player(object):
             except ValueError:
                 print "Try again, that didn't seem like a usable number!"
 
-    def guess_and_check(self):
-        guess_row = self.input_guess("Guess Row:")
-        guess_col = self.input_guess("Guess Col:")
-        guess_total = [guess_row] + [guess_col]
-        if (guess_row < 0 or guess_row > 6) or (guess_col < 0 or guess_col > 6):
+    @staticmethod
+    def guess_and_check(guess_total, player):
+        if (guess_total[0] < 0 or guess_total[0] > 6) or (guess_total[1] < 0 or guess_total[1] > 6):
             return "Oops, that's not even in the ocean."
-        elif self.board.board[guess_row][guess_col] == "O":
+        elif player.board.board[guess_total[0]][guess_total[1]] == "O":
             return "You guessed that one already."
-        for ship in self.ships:
+        for ship in player.ships:
             if guess_total in ship.location and guess_total not in ship.hits:
                 if guess_total not in ship.hits:
                     ship.hits.append(guess_total)
+                    print ship.hits
                 if ship.hits == ship.location:
-                    self.sunk.append(1)
-                    if sum(self.sunk) == len(self.ships):
-                        self.board.board[guess_row][guess_col] = "X"
-                        return 0
+                    player.sunk.append(1)
+                    if sum(player.sunk) == len(player.ships):
+                        player.board.board[guess_total[0]][guess_total[1]] = "X"
+                        return "You sunk their last BattleShip!"
                     elif ship.size == len(ship.hits):
-                        self.board.board[guess_row][guess_col] = "X"
+                        player.board.board[guess_total[0]][guess_total[1]] = "X"
                         return "You sunk a Battlship!"
                 else:
-                    self.board.board[guess_row][guess_col] = "X"
+                    player.board.board[guess_total[0]][guess_total[1]] = "X"
                     return "Hit!"
-            elif guess_total not in self.board.placed_ships:
-                self.board.board[guess_row][guess_col] = "O"
+            elif guess_total not in player.board.placed_ships:
+                player.board.board[guess_total[0]][guess_total[1]] = "O"
                 return "You missed my battleship!"
         return "You guessed and hit that one already"
 
+
 class Game(object):
     def __init__(self):
+        self.player_name = []
         self.players = []
 
-    def determine_player_count(self):
-        while True:
-            player_count = Player.input_guess("1 or 2 players?:")
-            if player_count > 0 or player_count < 3:
-                player = [Player("Player " + str(i + 1)) for i in range(player_count)]
-                for i in player:
-                    self.players.append(i)
-                break
-
-    def generate_assets(self):
-        for player in self.players:
+    @staticmethod
+    def generate_assets(player):
             player.board.generate_board()
-            player.build(0,SmallShip)
-            player.build(0,MediumShip)
-            player.build(12,LargeShip)
+            player.build(0, SmallShip)
+            player.build(0, MediumShip)
+            player.build(1, LargeShip)
             player.generate_ships()
 
-    def play_game(self):
-        for turn in range(50):
-            x = turn % len(self.players)
-            print self.players[x].name
-            self.players[x].board.print_board()
-            print self.players[x].board.placed_ships
-            outcome = self.players[x].guess_and_check()
-            if outcome == 0:
-                print "You sunk their last Battlship! " + self.players[x].name + " Wins!"
+    header_struct = struct.Struct('!I')
+
+    @staticmethod
+    def recvall(sock, length):
+        blocks = []
+        while length != 0:
+            block = sock.recv(length)
+            if not block:
+                raise EOFError('socket closed with %d bytes left'
+                               ' in this block'.format(length))
+            length -= len(block)
+            blocks.append(block)
+        return b''.join(blocks)
+
+    def get_block(self, sock):
+        data = self.recvall(sock, self.header_struct.size)
+        (block_length,) = self.header_struct.unpack(data)
+        return self.recvall(sock, block_length)
+
+    def put_block(self, sock, message):
+        block_length = len(message)
+        sock.send(self.header_struct.pack(block_length))
+        sock.send(message)
+
+    def get_data(self, conn):
+        while True:
+            block = self.get_block(conn)
+            if not block:
                 break
-            else:
-                print outcome
+            try:
+                return list(struct.unpack('!5s2I', block))
+            except Exception:
+                return block
+
+    def get_player_count(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('', 6668))
+        sock.listen(5)
+        print('Listening at', sock.getsockname())
+        print('Waiting for a connection')
+
+        def client_thread(conn):
+                data = self.get_data(conn)
+                print data[0:2]
+                if data[0] == "count":
+                    print self.players[0].board.placed_ships
+                    conn.close()
+                elif data[0] == "guess":
+                    guess_total = data[1:3]
+                    self.put_block(conn, self.players[0].guess_and_check(guess_total, self.players[0]))
+                    self.put_block(conn, b'')
+                    print guess_total
+                    conn.close()
+                else:
+                    print data
+                    print self.player_name
+                    if data not in self.player_name:
+                        new_player = Player(data)
+                        self.players.append(new_player)
+                        self.generate_assets(new_player)
+                        self.put_block(conn, "good")
+                        self.put_block(conn, b'')
+                    else:
+                        self.put_block(conn, "nope")
+                        self.put_block(conn, b'')
+                conn.close()
+
+        while True:
+            conn, addr = sock.accept()
+            thread.start_new_thread(client_thread, (conn,))
 
     def start(self):
-        self.determine_player_count()
-        self.generate_assets()
-        self.players[0].show_ship_locations()
-        self.play_game()
-
-
-
-
-print "Let's play Battleship!"
+        for turn in range(50):
+            self.get_player_count()
 
 game1 = Game()
 game1.start()
